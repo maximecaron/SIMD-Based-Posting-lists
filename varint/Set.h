98a95027af79721d8249a34b396a027bedcce814
Maxime Caron <maxime.caron@gmail.com>
2012-09-17 19:22:45 -0400

    update set interface

--- varint/Set.h
+++ varint/Set.h
@@ -5,13 +5,14 @@ public:
 		virtual int docID() = 0;
 		virtual int getBlockIndex(int docIdIndex) = 0;
 		virtual int nextDoc() = 0;
+		virtual int Advance(int target) = 0;
 	};
-	virtual Iterator* iterator()  = 0;
+	virtual shared_ptr<Iterator> iterator()  const = 0;
 	virtual void addDocs(unsigned int docids[],size_t start,size_t len)= 0;
 	virtual void addDoc(unsigned int docId) = 0;
 	virtual void compact() = 0;
 	virtual void flush() = 0;
 	
 	//Number of docIds in the set
-	virtual bool find(unsigned int target) = 0;
+	virtual bool find(unsigned int target) const = 0;
 };
